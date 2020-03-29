130aa501abedbba2650c7ed29a1edb0f93d61b7e
stormkirill98 <stormkirill98@gmail.com>
2020-03-29 20:46:38 +0300

    print file

--- src/main/kotlin/PrintFile.kt
+++ src/main/kotlin/PrintFile.kt
@@ -2,6 +2,9 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import java.io.File
+
+const val SKIP_PREFIX = "#skip"
 
 class PrintFilePlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -11,7 +14,25 @@ class PrintFilePlugin : Plugin<Project> {
 
 open class PrintFileTask : DefaultTask() {
     @TaskAction
-    fun printFile() {
-        println("Hello World")
+    fun printFile(filePath: String) {
+        val file = File(filePath)
+
+        var countSkipLines = 0
+        file.forEachLine { line ->
+            if (countSkipLines > 0) { // skip line
+                countSkipLines--
+            } else {
+                val prefix = getStartPrefixValue(line)
+                if (prefix == null) { // print line
+                    println(line)
+                } else { // get skip lines number
+                    countSkipLines = getCountSkipLines(prefix)
+                }
+            }
+        }
     }
+
+    private fun getStartPrefixValue(s: String) = Regex("^$SKIP_PREFIX \\d+").find(s)?.value
+
+    private fun getCountSkipLines(s: String) = s.substringAfter(SKIP_PREFIX).trim().toInt()
 }
